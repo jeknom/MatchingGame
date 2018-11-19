674544a66d3e8de6fd5560381b6c38a5334f5f41
Johannes Palvanen <palvanenj@gmail.com>
2018-11-19 20:58:43 +0200

    Project files

---
+++ MatchingGame/Assets/Scripts/Block.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType
+{
+	Blue,
+	Green,
+	Red,
+	Yellow
+}
+
+public class Block : MonoBehaviour 
+{
+	public Vector3 Coodinates;
+	private BlockType Colour;
+	public List<Material> Materials = new List<Material>();
+
+	private void Start ()
+	{
+		Colour = (BlockType)UnityEngine.Random.Range(0, 4);
+		GetComponent<MeshRenderer>().material = Materials[(int)Colour];
+	}
+
+	public static void ActivateBlock(GameObject block, Vector3 coordinates)
+	{
+		block.GetComponent<Rigidbody>().position = coordinates;
+		block.GetComponent<Rigidbody>().useGravity = true;
+	}
+}
---
+++ MatchingGame/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour 
+{
+	public GameObject BlockPrefab;
+	public GameObject[,] BlockGrid = new GameObject[8, 8];
+
+	private void Start () 
+	{
+		for (int y = 0; y < 8; y++)
+		{
+			for (int x = 0; x < 8; x++)
+			{
+				BlockGrid[y, x] = Instantiate(BlockPrefab, new Vector3(0.0f, 20.0f, 10.0f), Quaternion.identity);
+				Block.ActivateBlock(BlockGrid[y, x], new Vector3(x - 4, y + 10 + x, 10));
+			}
+		}
+	}
+
+	private void Update ()
+	{
+		
+	}
+}

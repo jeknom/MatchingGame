36331dfb78eac76fe0acbcc786fe0b181609be6a
Johannes Palvanen <palvanenj@gmail.com>
2018-11-28 19:08:41 +0200

    Revamping

--- MatchingGame/Assets/Scripts/GameLogic.cs
+++ MatchingGame/Assets/Scripts/GameLogic.cs
@@ -7,16 +7,21 @@ public class GameLogic : MonoBehaviour
 
 	private void Start() 
 	{
-		for (int y = 0; y < grid.Height; y++)
-			for (int x = 0; x < grid.Width; x++)
+		for (int x = 0; x < grid.Width; x++)
+			for (int y = 0; y < grid.Height; y++)
 			{
-				grid.Blocks[x, y] = Instantiate(block);
-				grid.Move(grid.Blocks[x, y], x, y);
+				grid.Blocks[x].Add(Instantiate(block));
+				grid.Move(grid.Blocks[x][y], x, y);
 			}
 	}
 
 	private void Update() 
 	{
-		grid.DestroyTarget(new Mouse());
+		var mouse = new Mouse();
+
+		if (mouse.GetInteraction() != null)
+			grid.RemoveBlock(mouse.GetInteraction().GetComponent<Block>());
+
+		grid.Cascade();
 	}
 }
--- MatchingGame/Assets/Scripts/Grid.cs
+++ MatchingGame/Assets/Scripts/Grid.cs
@@ -1,30 +1,46 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid
 {
     private const int _width = 5;
     private const int _height = 6;
-    
-    private IBlock[,] _blockGrid = new IBlock[_width, _height];
+    private List<List<IBlock>> _blockGrid = new List<List<IBlock>>();
     
     public int Width { get { return _width; } }
     public int Height { get { return _height; } }
-    public IBlock[,] Blocks { get { return _blockGrid; } }
+    public List<List<IBlock>> Blocks { get { return _blockGrid; } }
+
+    public Grid()
+    {
+        for (var x = 0; x < _width; x++)
+            _blockGrid.Add(new List<IBlock>());
+    }
 
     public void Move(IBlock block, int x, int y)
     {
         block.GetObject().transform.position = new Vector3 (x, y, 0);
     }
 
-    public void DestroyTarget(IPlayerControls controls)
-	{
-		var target = controls.GetInteraction();
+    public void RemoveBlock(IBlock block)
+    {
+        foreach (var row in _blockGrid)
+        {
+            var target = row.SingleOrDefault(b => b == block);
+            
+            if (target != null)
+            {
+                target.Destroy();
+                row.Remove(target);
+            }
+        }
+    }
 
-		if (target != null && target.gameObject.tag == "Block")
-		{
-            _blockGrid[target.GetComponent<IBlock>().x, target.GetComponent<IBlock>().y] = null;
-			MonoBehaviour.Destroy(target);
-		}
-	}
+    public void Cascade()
+    {
+        foreach (var row in _blockGrid)
+            row.ForEach(b => b.GetObject().GetComponent<Transform>().position = new Vector3(_blockGrid.IndexOf(row), row.IndexOf(b)));
+    }
 }

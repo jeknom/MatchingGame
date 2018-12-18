6a82f5dd9a42d48b4c8410e003d2e43a530c99ab
Johannes Palvanen <palvanenj@gmail.com>
2018-12-18 22:22:40 +0200

    Added an objective counter

---
+++ MatchingGame/Assets/Scripts/Gameplay/ObjectiveCounter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace MatchingGame
+{
+    public class ObjectiveCounter
+    {
+        private int moves;
+        private int squareCount;
+        private int blackBombCount;
+
+        public int Moves 
+        { 
+            get { return moves; } 
+            set { moves = value; }
+        }
+        public int SquareCount { get { return squareCount; } set { squareCount = value; } }
+        public int BlackBombCount { get { return blackBombCount; } set { blackBombCount = value; } }
+
+        public ObjectiveCounter()
+        {
+            this.moves = Random.Range(10, 20);
+            this.squareCount = Random.Range(10, 100);
+            this.blackBombCount = Random.Range(10, 30);
+        }
+
+        public ObjectiveCounter(int moves, int squareCount, int blackBombCount)
+        {
+            this.moves = moves;
+            this.squareCount = squareCount;
+            this.blackBombCount = blackBombCount;
+        }
+    }
+}

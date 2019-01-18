097469aa32967549009312177dbc619918d1a3cf
Johannes Palvanen <palvanenj@gmail.com>
2019-01-18 16:10:52 +0200

    Add UI Move & Objective Counters

---
+++ MatchingGame/Assets/Scripts/Unity/EditorGizmos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatchingGame
+{
+    public class EditorGizmos : MonoBehaviour
+    {
+        [SerializeField] private int gridWidth = 6;
+        [SerializeField] private int gridHeight = 6;
+
+        private void OnDrawGizmos() 
+        {
+            var cellGrid = new CellGrid(gridWidth, gridHeight);
+            GridOperation.Fill(cellGrid);
+
+            foreach (var column in cellGrid.Columns)
+                foreach (var cell in column)
+                {
+                    var point = GridQuery.ToPoint(cellGrid, cell);
+                    var center = new Vector3(point.x, point.y);
+                    var size = new Vector3(1, 1, 1);
+                    
+                    Gizmos.color = Color.yellow;
+                    Gizmos.DrawWireCube(center, size);
+                }
+        }
+    }
+}
